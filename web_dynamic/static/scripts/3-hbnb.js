4dbff3eaf6f330b6edf8c1cc9901cb56b7f66b1d
IgbeAjaga <ajagaigbe144@gmail.com>
2023-10-05 21:31:03 +0000

    task 4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    // Fetch places from the API
+    $.ajax({
+        url: 'http://0.0.0.0:5001/api/v1/places_search',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({}),
+        success: function (data) {
+            // Handle the data and create <article> tags for each Place
+            var placesSection = $('section.places');
+            placesSection.empty(); // Clear existing content
+
+            data.forEach(function (place) {
+                // Create an <article> tag for each Place
+                var article = $('<article></article>');
+
+                // Customize the content of the <article> tag based on Place details
+                // Example: article.html('<h2>' + place.name + '</h2>');
+
+                // Append the <article> tag to the placesSection
+                placesSection.append(article);
+            });
+        }
+    });
+
+});
